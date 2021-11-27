47b6b3aa7c86276e933b8123787a073c1205595a
xNewz <pargornruasijan@gmail.com>
2021-11-27 23:04:16 +0700

    Add library

---
+++ main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.stage.Stage;
